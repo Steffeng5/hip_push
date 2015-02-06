a539fb61df203943d7a894bdff3bf7ea161a9c7d
Steffen Genauck <Steffen@klabuster.com>
2015-02-06 17:54:41 +0100

    initial commit

---
+++ auth_complete.php
@@ -0,0 +1,31 @@
+<?php
+$config = yaml_parse(file_get_contents("config.yaml"));
+
+$code = htmlspecialchars($_GET["code"]);
+
+$postdata = http_build_query(
+    array(
+        'grant_type' => 'authorization_code',
+        'client_id' => $config['pushbullet_client_id'],
+        'client_secret' => $config['pushbullet_client_secret'],
+        'code' => $code
+    )
+);
+
+$opts = array('http' =>
+    array(
+        'method'  => 'POST',
+        'content' => $postdata
+    )
+);
+
+$context  = stream_context_create($opts);
+$result = file_get_contents('https://api.pushbullet.com/oauth2/token', false, $context);
+
+$token = json_decode($result);
+
+$access_token_file = fopen($config['access_token_file_path'], "a") or die("Unable to open file!");
+fwrite($access_token_file, $token->access_token.PHP_EOL);
+fclose($access_token_file);
+
+?>
---
+++ hip.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby -wKU
+require 'rubygems'
+require 'mechanize'
+require 'yaml'
+
+config = YAML.load_file('config.yaml')
+
+a = Mechanize.new
+a.get(config['hip_url']) do |page|
+  # Submit the login form
+  # puts page.content
+  dashboard = a.current_page.form_with(name: 'loginform') do |f|
+  
+  	f['asdf'] = config['user']
+    f['fdsa'] = config['password']
+  	
+  end.submit
+
+  dashboard   = a.click(dashboard.link_with(text: /fungsverwaltung/))
+  dashboard   = a.click(dashboard.link_with(text: /Notenspiegel/))
+  dashboard   = a.click(dashboard.link_with(text: /Abschluss: Bachelor/))
+  grades_page = a.click(dashboard.link_with(text: /dualer Bachelor-Studiengang/))
+
+  document      = Nokogiri::HTML(grades_page.content)
+  all_modules   = document.xpath('//form/table[2]').search('tr')
+  known_modules = File.read('module.txt', {encoding: "UTF-8"})
+
+  all_modules.each do |line|
+    status = line.at_xpath('td[5]/text()').to_s.strip
+    
+    if (status != "angemeldet")
+      modul = line.at_xpath('td[2]/text()').to_s.strip
+      unless (modul.empty? or known_modules.include?(modul))
+        open('module.txt', 'a:UTF-8') do |file|
+          file.puts modul
+        end
+
+        # Notify students
+        ausgabe = "Neue Noten im HIP: " + modul
+        #Pushover
+        a.post('https://api.pushover.net/1/messages.json', {
+          "token" => config['pushover_token'],
+          "user" => config['pushover_user'],
+          "title" => "Neue Noten!",
+          "message" => modul
+        })
+
+        #Notify others
+        IO.foreach config['access_token_file_path'] do |access_token|
+          a.add_auth('https://api.pushbullet.com', access_token.strip, '')
+          a.post('https://api.pushbullet.com/v2/pushes', {
+            "type" => "note",
+            "title" => "Neue Noten!",
+            "body" => modul
+          })
+        end
+      end
+    end
+  end
+end
